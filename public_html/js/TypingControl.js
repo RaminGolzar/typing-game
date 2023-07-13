b3c7b7906815528588de5f8f02f9959ebb89065c
Eraj <eraj@email.ir>
2023-07-13 21:23:51 +0430

    Writing some properties in the TypeGame.js

---
+++ public_html/js/TypingControl.js
@@ -0,0 +1,18 @@
+$(document).ready (function () {
+//    $('body').keyup (function(e) {
+//        $('title').text(e.keyCode);
+//        let ss = 'abc';
+//        let subtractAsciiFromUtf_16 = 32;
+//        $('title').text(ss.charCodeAt(2) - 32);
+//    });
+    
+    const TypingControl = {
+        subtractAsciiFromUtf_16 : 32 ,
+        gameBox : document.getElementById ('game-box'); ,
+        textBlock : document.getElementById ('text-block'); ,
+        timesRed : 0 ,
+        
+        
+    };
+});
+
