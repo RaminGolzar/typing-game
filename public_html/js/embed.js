5bbae44dbe1e0bcfe270a8bab14f75a37ccf1565
Eraj <eraj@email.ir>
2023-07-18 19:14:41 +0430

    Debuging & editing the embed.js file

--- public_html/js/embed.js
+++ public_html/js/embed.js
@@ -1,52 +1,51 @@
-$(document).ready(function () {
-    const embed = {
-        /**
-         * Return a lorem ipsum where each char contained
-         * with a span tag
-         * 
-         * @param {int} wordCount
-         * @param {int} minLen
-         * @param {int} maxLen
-         * @returns {String}
-         */
-        run : function (wordCount = 1 , minLen = 0 , maxLen = 0) {
-            let genLorem = lorem.genLorem (wordCount , minLen , maxLen);
-            
-            let exploded = this.explode (genLorem);
-            
-            let embed = this.embed(exploded);
-            
-            return embed;
-        } ,
 
-        /**
-         * Convert the string to an array
-         * 
-         * @param {type} lorem
-         * @returns {unresolved}
-         */
-        explode : function (lorem) {
-            return lorem.split ('');
-        } ,
+const embed = {
+    /**
+     * Return a lorem ipsum where each char contained
+     * with a span tag
+     * 
+     * @param {int} wordCount
+     * @param {int} minLen
+     * @param {int} maxLen
+     * @returns {String}
+     */
+    run : function (loremIpsum) {
+        alert ('{ Ok }');
+        let exploded = this.explode (loremIpsum);
 
-        /**
-         * Enclosing each char in a span tag
-         * 
-         * @param {array} arrayLorem
-         * @returns {Array}
-         */
-        embed : function (arrayLorem) {
-            let result = [];
-            
-            for (var i = 0; i < arrayLorem.length ; i++) {
-                let elem = $('<span></span>');
-                
-                elem.text(arrayLorem [i]);
-                
-                result[i] = elem;
-            }
-            
-            return result;
-        } 
-    };
-});
+        let embed = this.embeding (exploded);
+
+        return embed;
+    } ,
+
+    /**
+     * Convert the string to an array
+     * 
+     * @param {type} lorem
+     * @returns {unresolved}
+     */
+    explode : function (lorem) {
+        return lorem.split ('');
+    } ,
+
+    /**
+     * Enclosing each char in a span tag
+     * 
+     * @param {array} arrayLorem
+     * @returns {Array}
+     */
+    embeding : function (arrayLorem) {
+        let result = [];
+
+        for (var i = 0; i < arrayLorem.length ; i++) {
+            let elem = $('<span></span>');
+
+            elem.text(arrayLorem [i]);
+
+            result[i] = elem;
+        }
+
+        return result;
+    } 
+};
+
