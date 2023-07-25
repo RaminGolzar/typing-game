f3b4f669a21fbe8ec241dfcf0e0661056ae0e96c
Eraj <eraj@email.ir>
2023-07-25 19:13:01 +0430

    I wrote the preliminary codes in the Timer.js

--- public_html/js/Timer.js
+++ public_html/js/Timer.js
@@ -1,3 +1,36 @@
 const Timer = {
+    timeCounter : 0 ,
     
+    maxTime : 60 ,
+    
+    secondKeeper : 1000 ,
+    
+    startTime : function () {
+        let timer = setInterval (function () {
+            this.scheduledWork ();
+        } , this.secondKeeper);
+    } ,
+    
+    scheduledWork : function () {
+        if (! this.timeOverflow()) {
+            this.timeCounter++;
+            
+        }
+    } ,
+    
+    timeOverflow : function () {
+        return this.timeCounter <= this.maxTime ? false : true;
+    } ,
+    
+    run : function () {
+        
+    } ,
+    
+    stop : function () {
+        
+    } ,
+    
+    reset : function () {
+        
+    } 
 };
