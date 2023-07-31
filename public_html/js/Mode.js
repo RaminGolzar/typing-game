e75170f928ed85aed994387f2065cbbab6376287
Eraj <eraj@email.ir>
2023-07-31 19:23:45 +0430

    I wrote the functions of the is category in the Mode.js

--- public_html/js/Mode.js
+++ public_html/js/Mode.js
@@ -5,19 +5,39 @@ const Mode = {
         this.mode = 'start';
     } ,
     
+    isStartMode : function () {
+        return this.mode === 'start' ? true : false;
+    } ,
+    
     setTypingMode : function () {
         this.mode = 'typing';
+    } ,
+    
+    isTypingMode : function () {
+        return this.mode === 'typing' ? true : false;
     } ,
     
     setEndTimeMode : function () {
         this.mode = 'endTime';
     } ,
     
+    isEndTimeMode : function () {
+        return this.mode === 'endTime' ? true : false;
+    } ,
+    
     setTypedMode : function () {
         this.mode = 'typed';
+    } ,
+    
+    isTypedMode : function () {
+        return this.mode === 'typed' ? true : false;
     } ,
     
     setGameOverMode : function () {
         this.mode = 'gameOver';
     } ,
+    
+    isGameOverMode : function () {
+        return this.mode === 'gameOver' ? true : false;
+    } 
 };
