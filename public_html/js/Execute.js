50fc694f25ba1946c4c5727da79bbebc8509f997
Eraj <eraj@email.ir>
2023-08-01 19:11:28 +0430

    Complete coding in the Execute.js

--- public_html/js/Execute.js
+++ public_html/js/Execute.js
@@ -5,6 +5,46 @@ $('body').keypress (function (keyEvent) {
 $('#start').click(function() {
     $(this).hide ();
     
+    baseStartActions ();
+});
+
+$('#stop , #again').click(function() {
+    resetGame();
+    
+    $('#start').show();
+    
+    baseStopActions ($(this));
+});
+
+$('#next').click(function() {
+    baseStopActions ($(this));
+    
+    Level.goNext();
+    
+    Timer.reset();
+    
+    baseStartActions();
+});
+
+function resetGame () {
+    Level.reset();
+    
+    RedSign.reset();
+    
+    Timer.reset();
+    
+    Record.reset();
+}
+
+function baseStopActions (eventObject) {
+    Mode.setStop();
+    
+    $('#text-block').hide();
+    
+    eventObject.parent().hide();
+}
+
+function baseStartActions () {
     Mode.setStart ();
     
     $('#text-block').show ();
@@ -12,4 +52,8 @@ $('#start').click(function() {
     TypingControll.newTextBlock();
     
     Timer.run ();
-});
+}
+
+const Execute  = {
+    
+};
