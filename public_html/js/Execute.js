b09c56a9130891f389ea98299507fad97aafb575
Eraj <eraj@email.ir>
2023-08-01 17:16:37 +0430

    I wrote the body & start events in the Execute.js file

--- public_html/js/Execute.js
+++ public_html/js/Execute.js
@@ -1,7 +1,15 @@
-const Execute = {
-    
-};
+$('body').keypress (function (keyEvent) {
+    TypingControll.run (keyEvent);
+});
 
 $('#start').click(function() {
+    $(this).hide ();
     
+    Mode.setStart ();
+    
+    $('#text-block').show ();
+    
+    TypingControll.newTextBlock();
+    
+    Timer.run ();
 });
